dcd99ad23ff11606a24cfa5b2ea7137641519f18
Shiva9361 <shivadharshansankar936@gmail.com>
2024-09-17 13:42:45 +0530

    fix: tar

--- install_building_tools.sh
+++ install_building_tools.sh
@@ -3,6 +3,6 @@ set -e
 
 sudo apt-get install gcc g++ gcc-multilib g++-multilib make
 
-cd ..
-#wget https://www.fit.hcmus.edu.vn/~ntquan/os/assignment/mips-decstation.linux-xgcc.gz
-tar zxvf mips-decstation.linux-xgcc.gz
+# cd ..
+# #wget https://www.fit.hcmus.edu.vn/~ntquan/os/assignment/mips-decstation.linux-xgcc.gz
+# tar zxvf mips-decstation.linux-xgcc.gz
